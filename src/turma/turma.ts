69141def1838e30911143d08c54a196fc08b4763
Mariana Herreros <maribranding@gmail.com>
2021-10-06 20:53:15 -0300

    Adicionando vetor na turma

--- src/turma/turma.ts
+++ src/turma/turma.ts
@@ -1,5 +1,6 @@
 import { ALAN_JHONNES } from "../alunos/alan-jhonnes";
+import { MARIANA_HERREROS } from "../alunos/marianaherreros";
 
 export const turma: string[] = [
-    ALAN_JHONNES,
+    ALAN_JHONNES, MARIANA_HERREROS
 ]
