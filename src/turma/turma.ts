9438a7eb7d6a0a98580d6eb761ae9d0b03170f47
Ana Claudia <anac.almeidasouza@gmail.com>
2021-10-06 21:10:05 -0300

    adicionado import anaclaudia

--- src/turma/turma.ts
+++ src/turma/turma.ts
@@ -1,5 +1,7 @@
 import { ALAN_JHONNES } from "../alunos/alan-jhonnes";
+import { Ana_Claudia } from "../alunos/ana-claudia";
+
 
 export const turma: string[] = [
-    ALAN_JHONNES,
+    ALAN_JHONNES,Ana_Claudia
 ]
