3b2522247454e74ed85af0ed4e0e1e9259aa0df2
luelen-cavalheiro <luelencavalheiro@hotmail.com>
2021-10-12 23:00:02 -0300

    adicionando nome na lista

--- src/turma/turma.ts
+++ src/turma/turma.ts
@@ -3,6 +3,7 @@ import { REBASE } from "../alunos/aluno-rebase";
 import { JHONNES_ALAN } from "../alunos/jhonnes-alan";
 import { STEFANI_BARROS} from "../alunos/stefani-barros";
 import { MARIANA_HERREROS } from "../alunos/marianaherreros";
+import { LUELEN_CAVALHEIRO } from "../alunos/luelen-cavalheiro";
 
 
 export const turma: string[] = [
@@ -10,5 +11,6 @@ export const turma: string[] = [
     JHONNES_ALAN,
     STEFANI_BARROS,
     MARIANA_HERREROS,
+    LUELEN_CAVALHEIRO,
     REBASE,
 ];
